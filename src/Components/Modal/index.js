f5fbf9d96985b580b744aa053e8a88dc85432034
Diego Bousfield <diegobousfield@gmail.com>
2019-01-15 17:48:55 -0200

    Add modal css styles

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactModal from 'react-modal';
 import PropTypes from 'prop-types';
 
+import 'font-awesome/css/font-awesome.css';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -10,12 +12,15 @@ import { Creators as modalActions } from '../../store/ducks/modal';
 
 import './styles.css';
 
-ReactModal.setAppElement('#root');
+ReactModal.setAppElement(document.getElementById('root'));
 
 class Modal extends Component {
   static propTypes = {
     modal: PropTypes.shape({
       show: PropTypes.bool.isRequired,
+    }).isRequired,
+    users: PropTypes.shape({
+      loading: PropTypes.bool.isRequired,
     }).isRequired,
     closeModal: PropTypes.func.isRequired,
     addUserRequest: PropTypes.func.isRequired,
@@ -24,6 +29,8 @@ class Modal extends Component {
   state = {
     userInput: '',
   };
+
+  handleInput = e => this.setState({ userInput: e.target.value });
 
   handleSubmit = (e) => {
     const {
@@ -34,40 +41,58 @@ class Modal extends Component {
 
     e.preventDefault();
 
+    if (!userInput) return;
+
     addUserRequest(userInput, coordinates);
 
     this.setState({ userInput: '' });
   };
 
+  handleClose = () => {
+    const { closeModal } = this.props;
+
+    this.setState({ userInput: '' });
+    closeModal();
+  };
+
   render() {
-    const { modal, closeModal } = this.props;
+    const { modal, users } = this.props;
     const { userInput } = this.state;
+
     return (
-      <ReactModal className="add-user" isOpen={modal.show} onRequestClose={closeModal}>
+      <ReactModal
+        className="add-user"
+        overlayClassName="overlay"
+        isOpen={modal.show}
+        onRequestClose={this.handleClose}
+      >
         <h3>Adionar novo usuário</h3>
-        <form onSubmit={this.handleSubmit}>
+
+        <form className="form" onSubmit={this.handleSubmit}>
           <input
             type="text"
-            autoFocus
             placeholder="Usuário no Github"
             value={userInput}
-            onChange={e => this.setState({ userInput: e.target.value })}
+            onChange={this.handleInput}
           />
+          <div className="buttons-wrapper">
+            <button type="button" onClick={this.handleClose}>
+              Cancelar
+            </button>
 
-          <button type="button" className="cancel" onClick={closeModal}>
-            Cancelar
-          </button>
-          <button type="submit" className="submit">
-            Salvar
-          </button>
+            <button type="submit">
+              {users.loading ? <i className="fa fa-spinner fa-pulse" /> : 'Salvar'}
+            </button>
+          </div>
         </form>
       </ReactModal>
     );
   }
 }
 
-const mapStateToProps = ({ modal }) => ({
-  modal,
+const mapStateToProps = state => ({
+  modal: state.modal,
+  users: state.users,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({ ...userActions, ...modalActions }, dispatch);
