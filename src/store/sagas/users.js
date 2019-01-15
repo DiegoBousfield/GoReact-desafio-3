97091caf894ac91b7e8172866d1a24cea87938fa
Diego Bousfield <diegobousfield@gmail.com>
2019-01-15 17:49:49 -0200

    Add functionality to check duplicated users

--- src/store/sagas/users.js
+++ src/store/sagas/users.js
@@ -2,10 +2,19 @@ import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Creators as UserActions } from '../ducks/users';
+import { Creators as ModalActions } from '../ducks/modal';
 
 export function* addUser(action) {
   try {
     const { data } = yield call(api.get, `/users/${action.payload.user}`);
+
+    const isDuplicated = yield select((state) => {
+      state.users.data.find(user => user.id === data.id);
+    });
+
+    if (isDuplicated) {
+      yield put(UserActions.addUserFailure('Usuário ja existe!'));
+    }
 
     const userData = {
       id: data.id,
@@ -17,5 +26,7 @@ export function* addUser(action) {
     yield put(UserActions.addUserSuccess(userData));
   } catch (error) {
     yield put(UserActions.addUserFailure('Erro ao adicionar o usuário'));
+  } finally {
+    yield put(ModalActions.closeModal());
   }
 }
