712b8c0e1a158f1ee37830bbf5b502f45d5d007d
Diego Bousfield <diegobousfield@gmail.com>
2019-02-03 11:07:32 -0200

    Render react-toastfy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-
 import './config/reactotron';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import 'font-awesome/css/font-awesome.css';
+import './styles.css';
+
+import { Provider } from 'react-redux';
 import store from './store';
 
 import Routes from './routes';
@@ -9,6 +15,7 @@ import Routes from './routes';
 const App = () => (
   <Provider store={store}>
     <Routes />
+    <ToastContainer />
   </Provider>
 );
 
